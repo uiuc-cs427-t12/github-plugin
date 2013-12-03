4b2cee7d784cc91fa682ae00f3c079ccabeb24eb
Perry Huang <perry.huang@gmail.com>
2013-12-02 19:37:01 -0600

    Add user story 1 test cases.

--- src/test/java/com/cloudbees/jenkins/GitHubPushTriggerConfigSubmitTest.java
+++ src/test/java/com/cloudbees/jenkins/GitHubPushTriggerConfigSubmitTest.java
@@ -15,63 +15,71 @@ import org.kohsuke.stapler.Stapler;
  */
 public class GitHubPushTriggerConfigSubmitTest extends HudsonTestCase {
 
-    private static final String WEBHOOK_URL = "http://jenkinsci.example.com/jenkins/github-webhook/";
+	private static final String WEBHOOK_URL = "http://jenkinsci.example.com/jenkins/github-webhook/";
+	public void testConfigSubmit_AutoManageHook() throws Exception {
 
-    public void testConfigSubmit_AutoManageHook() throws Exception {
+		WebClient client = configureWebClient();
+		HtmlPage p = client.goTo("configure");
+		HtmlForm f = p.getFormByName("config");
+		f.getInputByValue("auto").setChecked(true);
+		f.getInputByName("_.hookUrl").setChecked(true);
+		f.getInputByName("url").setValueAttribute(WEBHOOK_URL);
+		f.getInputByName("_.username").setValueAttribute("jenkins");
+		f.getInputByName("_.password").setValueAttribute("password");
+		submit(f);
 
-        WebClient client = configureWebClient();
-        HtmlPage p = client.goTo("configure");
-        HtmlForm f = p.getFormByName("config");
-        f.getInputByValue("auto").setChecked(true);
-        f.getInputByName("_.hookUrl").setChecked(true);
-        f.getInputByName("url").setValueAttribute(WEBHOOK_URL);
-        f.getInputByName("_.username").setValueAttribute("jenkins");
-        f.getInputByName("_.password").setValueAttribute("password");
-        submit(f);
+		GitHubPushTrigger.DescriptorImpl d = getDescriptor();
+		assertTrue(d.isManageHook());
+		assertEquals(new URL(WEBHOOK_URL), d.getHookUrl());
 
-        GitHubPushTrigger.DescriptorImpl d = getDescriptor();
-        assertTrue(d.isManageHook());
-        assertEquals(new URL(WEBHOOK_URL), d.getHookUrl());
+		List<Credential> credentials = d.getCredentials();
+		assertNotNull(credentials);
+		assertEquals(1, credentials.size());
+		Credential credential = credentials.get(0);
+		assertEquals("jenkins", credential.username);
+		assertEquals("password", Secret.toString(credential.password));
+	}
 
-        List<Credential> credentials = d.getCredentials();
-        assertNotNull(credentials);
-        assertEquals(1, credentials.size());
-        Credential credential = credentials.get(0);
-        assertEquals("jenkins", credential.username);
-        assertEquals("password", Secret.toString(credential.password));
-    }
+	public void testConfigSubmit_ManuallyManageHook() throws Exception {
 
-    public void testConfigSubmit_ManuallyManageHook() throws Exception {
+		WebClient client = configureWebClient();
+		HtmlPage p = client.goTo("configure");
+		HtmlForm f = p.getFormByName("config");
+		f.getInputByValue("none").setChecked(true);
+		submit(f);
 
-        WebClient client = configureWebClient();
-        HtmlPage p = client.goTo("configure");
-        HtmlForm f = p.getFormByName("config");
-        f.getInputByValue("none").setChecked(true);
-        submit(f);
+		GitHubPushTrigger.DescriptorImpl d = getDescriptor();
+		assertFalse(d.isManageHook());
+	}
 
-        GitHubPushTrigger.DescriptorImpl d = getDescriptor();
-        assertFalse(d.isManageHook());
-    }
+	public void testPushComment() {
+		GitHubPushTrigger test = new GitHubPushTrigger();
+		
+		assertEquals(test.getGHUserName(), "modong");
+		assertEquals(test.getGHTargetFolder(), "cs427");
+		assertEquals(test.getGHCommitID(), "7a0ae42d033742be8aa548de6a3ed45d8a5d663a");
+		assertEquals(test.getCommitComment(), "Test inside jenkins.");
+	}
 
-    private GitHubPushTrigger.DescriptorImpl getDescriptor() {
-        return (GitHubPushTrigger.DescriptorImpl) GitHubPushTrigger.DescriptorImpl.get();
-    }
+	private GitHubPushTrigger.DescriptorImpl getDescriptor() {
+		return (GitHubPushTrigger.DescriptorImpl) GitHubPushTrigger.DescriptorImpl.get();
+	}
 
-    private WebClient configureWebClient() {
-        WebClient client = new WebClient();
-        client.setThrowExceptionOnFailingStatusCode(false);
-        client.setCssEnabled(false);
-        client.setJavaScriptEnabled(true);
-        return client;
-    }
+	private WebClient configureWebClient() {
+		WebClient client = new WebClient();
+		client.setThrowExceptionOnFailingStatusCode(false);
+		client.setCssEnabled(false);
+		client.setJavaScriptEnabled(true);
+		return client;
+	}
 
-    // workaround
-    static {
-        Stapler.CONVERT_UTILS.register(new org.apache.commons.beanutils.Converter() {
+	// workaround
+	static {
+		Stapler.CONVERT_UTILS.register(new org.apache.commons.beanutils.Converter() {
 
-            public Secret convert(Class type, Object value) {
-                return Secret.fromString(value.toString());
-            }
-        }, Secret.class);
-    }
+			public Secret convert(Class type, Object value) {
+				return Secret.fromString(value.toString());
+			}
+		}, Secret.class);
+	}
 }
