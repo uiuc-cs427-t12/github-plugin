3eff38c5cfd706ddac7e3ba27a602e26c915dfb5
EricRen20 <quxcc5215@mobile-192-17-200-146.near.uiuc.edu>
2013-12-10 20:25:57 -0600

    update buildReportParser.java

--- src/test/java/com/cloudbees/jenkins/buildReportParserTest.java
+++ src/test/java/com/cloudbees/jenkins/buildReportParserTest.java
@@ -1,12 +1,12 @@
-package com.cloudbees.jenkins;;
- 
+package com.cloudbees.jenkins;
+
 import static org.junit.Assert.*;
- 
+
 import org.junit.BeforeClass;
 import org.junit.Test;
- 
+
 import com.cloudbees.jenkins.buildReportParser;
- 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,62 +15,63 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
- 
+
+
 /**
  * @author Pengyu Ren, Kiwook Lee
  *
  */
-public class testBuildReportParser {
- 
+public class buildReportParserTest {
+    
+    
     /**
+     *this method basically create a testjob in jenkins source folder so that it can be used in the following test cases.
      * @throws java.lang.Exception
      */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        //create temp jenkins source folder, and temp log file in that folder
-        //create a job folder
+       
         String home = System.getProperty("user.home");
         File testJob=new File(home+File.separator+".jenkins"+File.separator,"jobs");
         testJob.mkdir();
-        //create a job whose name is testjob. (it's actually a folder)
-        File  testJobName=new File(testJob.getAbsolutePath(),"testjob");
+        
+        File testJobName=new File(testJob.getAbsolutePath(),"testjob");
         testJobName.mkdir();
-        //create a nextBuildNumber file in the testjob folder
+        
         File nextBuildNumber=new File(testJobName.getAbsolutePath(),"nextBuildNumber");
         String content="2";
         BufferedWriter out;
         out= new BufferedWriter(new FileWriter(nextBuildNumber));
-            out.write(content, 0, content.length());
-         
+        out.write(content, 0, content.length());
+        
         out.close();
-        //create a build folder in testjob folder
+        
         File builds=new File(testJobName.getAbsolutePath(),"builds");
         builds.mkdir();
-        // create 1 folder in this folder
+        
         File one=new File(builds.getAbsolutePath(),"1");
         one.mkdir();
-        //create an initial log file in 1 folder
+        
         File log=new File(one.getAbsolutePath(),"log");
         copy("log_success",log,log.getAbsolutePath());
-          
-        //create 2 folder in this folder
+        
+        
         File two=new File(builds.getAbsolutePath(),"2");
         two.mkdir();
         File log2=new File(two.getAbsolutePath(),"log");
         copy("log_failture",log2,log2.getAbsolutePath());
-          
-        //System.out.println(home+File.separator+"Desktop");
-         
+        
+        
+        
     }
- 
+    
     @Test
     public void testGetUser() throws FileNotFoundException {
         String result=buildReportParser.getUser("testjob");
-         
+        
         assertEquals(result,"anonymous");
     }
- 
+    
     @Test
     public void testGetWorkSpace() throws FileNotFoundException {
         String result=buildReportParser.getWorkSpace("testjob");
@@ -93,7 +94,7 @@ public class testBuildReportParser {
     public void testGetCompleteReports() throws IOException {
         String result=buildReportParser.getcompleteReport("testjob");
         //System.out.println(result);
-         
+        
         assertEquals(result,expectedResult());
     }
     @Test
@@ -114,54 +115,63 @@ public class testBuildReportParser {
         //System.out.println(result);
         assertEquals(result," 38M/355M");
     }
-     
+    /**
+     *This method copy the log file, which is log-success and log-failture, into jenkins sourcefolder.
+     */
     private static void copy(String log, File result, String destPath) throws IOException {
         File dir = new File("");
         System.out.println("Path: "+dir.getAbsolutePath());
-        //System.out.println(dir.getp)
-        String source = dir.getAbsolutePath()+File.separator+log;
-   
+        
+        String source = dir.getAbsolutePath()+inProjectPath()+File.separator+log;
+        
         File fin = new File(source);
         FileInputStream fis = new FileInputStream(fin);
         BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-   
+        
         BufferedWriter out = new BufferedWriter(new FileWriter(result));
-   
+        
         String aLine = null;
         while ((aLine = in.readLine()) != null) {
-            //Process each line and add output to Dest.txt file
+            
             out.write(aLine);
             out.newLine();
         }
-   
-        // do not forget to close the buffer reader
+
         in.close();
-   
-        // close buffer writer
         out.close();
     }
+    /**
+     *This helper function is going to used in testGetCompleteReports().
+      It's going to test if the complete reports equals to what we expected.
+     */
     private static String expectedResult() throws IOException {
-         File dir = new File("");
-         System.out.println("lala:"+dir.getCanonicalPath());
-         System.out.println("Path: "+dir.getAbsolutePath());
-         String source = dir.getAbsolutePath()+File.separator+"log_success";
-    
-         File fin = new File(source);
-         FileInputStream fis = new FileInputStream(fin);
-         BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-    
-    
-         String aLine = null;
-         String output="";
-         while ((aLine = in.readLine()) != null) {
-             //Process each line and add output to Dest.txt file
-             output=output+aLine+"\n";
-         }
-    
-         // do not forget to close the buffer reader
-         in.close();
-   
-         return output;
+        File dir = new File("");
+        
+        String source = dir.getAbsolutePath()+inProjectPath()+File.separator+"log_success";
+        
+        File fin = new File(source);
+        FileInputStream fis = new FileInputStream(fin);
+        BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+        
+        
+        String aLine = null;
+        String output="";
+        while ((aLine = in.readLine()) != null) {
+            output=output+aLine+"\n";
+        }
+        
+        in.close();
+        
+        return output;
     }
+    /**
+     *this method will help to locate the correct path of the log_success and log_failture files. 
+     */
+    private static String inProjectPath() {
+        String inProjectPath = File.separator+"src"+File.separator+"test"+File.separator+File.separator+"java"+File.separator+"com"+File.separator+"cloudbees"+File.separator+"jenkins";
+        return inProjectPath;
+    }
+    
+
 }
     
